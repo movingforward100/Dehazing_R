89df324e8b1cfd8b426a491b636c6b40fb96d0b0
movingforward100 <155146977+movingforward100@users.noreply.github.com>
2024-03-23 06:56:48 -0400

    Delete FlashInternImage/models directory

--- FlashInternImage/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# --------------------------------------------------------
-# DCNv4
-# Copyright (c) 2024 OpenGVLab
-# Licensed under The MIT License [see LICENSE for details]
-# --------------------------------------------------------
-
-from .build import build_model
